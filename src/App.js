9f2206767517befe3899cdc14ea812821d347703
Alejandro <mendozag.alejandro@gmail.com>
2022-03-21 15:10:33 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState } from 'react';
+
+
+function App() {
+  const [tasks, setTasks] = useState([
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'Feb 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at school',
+    day: 'Feb 6th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food shooping',
+    day: 'Feb 5th at 2:30pm',
+    reminder: false,
+  }
+]);
+
+// Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task))
+}
+
+  return (
+    <div className='container'>
+        <Header />
+        <Tasks tasks={tasks} 
+          onDelete={deleteTask} 
+          onToggle={toggleReminder} /> 
+    </div>
+
+  )
+}
+
+export default App
